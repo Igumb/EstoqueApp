d2a3c9fed9d49a4a12ea69117fb9f763f57df9c4
igumb <ibuto@infisconsultoria.com.br>
2023-05-31 14:26:48 -0300

    primeiro commit

---
+++ EstoqueApp.Domain/Interfaces/Repositories/IProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using EstoqueApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueApp.Domain.Interfaces.Repositories
+{
+    public interface IProdutoRepository : IBaseRepository<Produto, Guid>
+    {
+
+    }
+}
+
+
